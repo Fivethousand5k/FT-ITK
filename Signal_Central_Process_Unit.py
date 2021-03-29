ff76dc824176fabbb7016b2a37cade7a56550690
Fivethousand-lenovo <1071824055@qq.com>
2021-03-29 22:04:16 +0800

    completed: 1.auxillary lines 2.padding sagittal and coronal images to the central part of each label screen.

---
+++ Signal_Central_Process_Unit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+    @Author Fivethousand
+    @Date 2021/3/29 13:27
+    @Describe 
+    @Version 1.0
+"""
+class SCPU():           #Signal_Central_Process_Unit
+    def __init__(self):
+        pass
+    def init_Signal_Channels(self):
